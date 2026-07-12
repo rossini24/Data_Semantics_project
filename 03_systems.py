0f4c26bc9e097753e3f7f215e97dcd964e4618a9
lorenzozanotti <lorenzo.lorenzan@gmail.com>
2026-07-12 18:22:40 +0200

    increase SPARQL timeout from 30 to 90 seconds

--- 03_systems.py
+++ 03_systems.py
@@ -325,7 +325,7 @@ class GraphRAG:
 
         thread = threading.Thread(target=run_query)
         thread.start()
-        thread.join(timeout=30)
+        thread.join(timeout=90)
 
         if thread.is_alive():
             return [{"error": "SPARQL query timed out after 30 seconds"}]
